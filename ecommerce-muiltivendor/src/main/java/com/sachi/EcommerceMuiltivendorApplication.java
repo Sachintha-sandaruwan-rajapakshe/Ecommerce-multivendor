00a630b47162bf15fda5d36858cd47e1ebafd683
Sachintha-sandaruwan-rajapakshe <126498862+Sachintha-sandaruwan-rajapakshe@users.noreply.github.com>
2025-09-04 18:12:07 +0530

    Initial commit

---
+++ ecommerce-muiltivendor/src/main/java/com/sachi/EcommerceMuiltivendorApplication.java
@@ -0,0 +1,13 @@
+package com.sachi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMuiltivendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMuiltivendorApplication.class, args);
+	}
+
+}
